53ba8ebc523dd185bd47a48e44cb4a809adad74b
bushing <bushing@7abf3ecb-30c5-952a-0715-4dbe46704d9e>
2010-04-02 03:36:19 +0000

    add GL660 GPIO primitives

--- trunk/gl.h
+++ trunk/gl.h
@@ -28,13 +28,17 @@
  
 */
 #ifndef GL_H__
-#define GL_G__    1
+#define GL_H__    1
 
 #define    GL_OK             0
 #define    GL_ELIBUSB       -1
 #define    GL_EOPEN         -2
 #define    GL_ESETCONFIG    -3
 #define    GL_ECLAIM        -4
+
+int gl_gpio_oe(unsigned int mask);
+int gl_gpio_read(void);
+int gl_gpio_write(unsigned int val);
 
 int gl_write_address(unsigned int address);
 int gl_write_data(unsigned int val);
